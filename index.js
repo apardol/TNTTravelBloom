4c56fe045b6fce34a5edf7f105c06dc76b2986f7
Adalberto Pardo <pardo524@gmail.com>
2024-11-25 16:49:49 -0500

    added fetch

---
+++ index.js
@@ -0,0 +1,48 @@
+const btnSearch = document.getElementById('btnSearch');
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function searchCondition() {
+    const inputSearch = document.getElementById('inputSearch').value.toLowerCase().trim();
+    const resultDiv = document.getElementById('recommendations');
+    resultDiv.innerHTML = '';
+
+    fetch('api.json')
+        .then(response => response.json())
+        .then(data => {
+
+            if (inputSearch === 'country') {
+                resultDiv.innerHTML += `<img src="${data.countries[0].cities[0].imageUrl}" alt="hjh">`;
+                resultDiv.innerHTML += `<h2>${data.countries[0].cities[0].name}</h2>`;
+
+                resultDiv.innerHTML += `<p> ${data.countries[0].cities[0].description}</p>`;
+                resultDiv.innerHTML += `<button>Visit</button>`;
+
+            }
+
+            // const condition = data.conditions.find(item => item.name.toLowerCase() === inputSearch);
+
+            // if (condition) {
+            //     const symptoms = condition.symptoms.join(', ');
+            //     const prevention = condition.prevention.join(', ');
+            //     const treatment = condition.treatment;
+
+            //     resultDiv.innerHTML += `<h2>${condition.name}</h2>`;
+            //     resultDiv.innerHTML += `<img src="${condition.imagesrc}" alt="hjh">`;
+
+            //     resultDiv.innerHTML += `<p><strong>Symptoms:</strong> ${symptoms}</p>`;
+            //     resultDiv.innerHTML += `<p><strong>Prevention:</strong> ${prevention}</p>`;
+            //     resultDiv.innerHTML += `<p><strong>Treatment:</strong> ${treatment}</p>`;
+            // } else {
+            //     resultDiv.innerHTML = 'Condition not found.';
+            // }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            resultDiv.innerHTML = 'An error occurred while fetching data.';
+        });
+}
+
+btnSearch.addEventListener('click', searchCondition);
